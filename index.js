03f1f292a39f746c3d16858bda8415e70d74c3d1
Justin <93454878+justinsavage49@users.noreply.github.com>
2023-02-28 00:11:39 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,28 +1,28 @@
 function headingChange() {
     //Capture main title element by ID for js reference
-    heading = document.getElementById("main-heading")
+    heading = document.getElementById("main-heading");
     //Set text of main title element 
-    heading.innerHTML = "This sentence was rendered from Javascript"   
-}
+    heading.innerHTML = "This sentence was rendered from Javascript" ;  
+};
 
 async function fetchText() {
     //Capture main title element by ID for js reference
-    heading = document.getElementById("main-heading")
+    heading = document.getElementById("main-heading");
 
     //await a response from the server at the address /server-text
-    let response = await fetch('/server-text')
-
-    //await response "text", then set main title to captured text
+    let response = await fetch('/server-text');
+    
+    //capture response as "text", then set main title to captured text
     await response.json()
         .then(text => {
-            heading.innerHTML = text
-        })
-}
+            heading.innerHTML = text;
+        });
+};
 
 function refresh() {
     //Refresh page to reset html title w/o button toggle
-    location.reload()
-}
+    location.reload();
+};
 
 
 
